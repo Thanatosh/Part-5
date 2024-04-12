e27f80e5f9592ba1b30eca4cfe1d909c40bab9c3
Thanatos <Thanatosh@gmail.com>
2024-04-12 08:36:46 +0300

    Part 5.14 Full Commit

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Renders content', () => {
@@ -13,4 +14,27 @@ test('Renders content', () => {
   expect(titleElement).toBeDefined()
   const authorElement = screen.getByText(new RegExp(blog.author, 'i'))
   expect(authorElement).toBeDefined()
+})
+
+test('Clicking the view button shows user, url and likes', async () => {
+  const blog = {
+    user: {
+      name: 'Joni Hostikka'
+    },
+    url: 'TestURL',
+    likes: 12
+  }
+
+  render(<Blog blog={blog} user={blog.user} url={blog.url} likes={blog.likes} />)
+
+  const event = userEvent.setup()
+  const button = screen.getByText('View')
+  await event.click(button)
+
+  const userElement = screen.getByText(new RegExp(blog.user.name, 'i'))
+  expect(userElement).toBeDefined()
+  const urlElement = screen.getByText(new RegExp(blog.url, 'i'))
+  expect(urlElement).toBeDefined()
+  const likesElement = screen.getByText(new RegExp(blog.likes, 'i'))
+  expect(likesElement).toBeDefined()
 })
